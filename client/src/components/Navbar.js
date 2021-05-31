f60ee5142656f4fa1edcf15670ed4404349f913b
Gourab Banik <gourabbanik2399@gmail.com>
2021-06-01 04:24:20 +0530

    React Register page UI  create

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
                 <NavLink className="navbar-brand" to="/">Navbar</NavLink>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Signup = () => {
     return (
-        <div>
-            <h1>hrllo Signup</h1>
-        </div>
+        <>
+            <div className="container">
+                <div className="title">Registration</div>
+                <div className="content">
+
+                    <form action="#">
+                        <div className="user-details">
+                            <div className="input-box">
+                                <span className="details">Full Name</span>
+                                <input type="text" name="name" placeholder="Enter your name" required autoComplete="off" />
+                            </div>
+                        
+                            <div className="input-box">
+                                <span className="details">Email</span>
+                                <input type="text" name="" placeholder="Enter your email" required autoComplete="off" />
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Phone Number</span>
+                                <input type="number" name="number" placeholder="Enter your number" required autoComplete="off" />
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Your Work</span>
+                                <input type="text" name="work" placeholder="Work" required autoComplete="off" />
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Password</span>
+                                <input type="password" name="password" placeholder="Enter your password" required autoComplete="off" />
+                            </div>
+                            <div className="input-box">
+                                <span className="details">Confirm Password</span>
+                                <input type="password" name="cpassword" placeholder="Confirm your password" required autoComplete="off" />
+                            </div>
+                        </div>
+                        
+                        <div className="button form-group">
+                            <input type="submit" className="signup" value="Register" />
+                        </div>
+                    </form>
+                    <div>Already registered? Go to <NavLink to="/login" >Login page</NavLink></div>
+                </div>
+            </div>
+
+
+
+        </>
     )
 }
 
