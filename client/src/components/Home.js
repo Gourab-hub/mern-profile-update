39d0ab6bb99b48762b79bc09ac0ea9d54a780708
Gourab Banik <gourabbanik2399@gmail.com>
2021-06-13 04:59:30 +0530

    React Home page Create

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div >
-            <h1>hrllo Home</h1>
-        </div>
+        <>
+        <center>
+        <div className="container-home">
+                <div>
+                    <p className="container-home-p">WELCOME</p>
+                    <h1 className="container-home-h1">We are the MERN developer</h1>
+                </div>
+            </div>
+        </center>
+     
+        </>
     )
 }
 
